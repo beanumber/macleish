afe1bb001db393535feb282d966a28e0e8b9024e
Ben Baumer <ben.baumer@gmail.com>
2015-11-12 14:49:27 -0500

    added wind speed

--- R/data.R
+++ R/data.R
@@ -1,15 +1,21 @@
-#' Weather data from Macleish Field Station
+#' Weather data from Macleish Field Stations
 #'
-#' @description data from Macleish from 2010 to present. 
+#' @description Weather data collected at the Macleish Field Station in Whately, 
+#' MA, from 2010 to present. 
 #'
-#' @details the description
+#' @details The Macleish Field Station is a remote outpost owned by Smith
+#' College and used for field research. There are two weather stations on the 
+#' premises. One is called \code{Whately} and the other is \code{Orchard}.
 #'
 #' @format A data frame with several hundred thousand row and 9 variables:
 #' \describe{
 #'   \item{when}{timestamp, recorded every 10 minutes}
 #'   \item{record}{?}
 #'   \item{Temp_C_Avg}{average temperature, in Celsius, over these 10 minutes}
+#'   \item{WSpd_mps}{wind speed}
 #' }
 #' @source These data are recorded at \url{http://www.smith.edu/ceeds/macleish.php}
 "whately"
+
+#' @rdname whately
 "orchard"
