672f3c772abc27a57eaac2608f3c4b487319f0b5
Ben Baumer <ben.baumer@gmail.com>
2016-03-09 16:35:54 -0500

    converted to ETL package

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -2,11 +2,35 @@
 #' 
 #' 
 #' @export
+#' @examples 
+#' 
+#' macleish <- etl("macleish")
+#' str(macleish)
+#' 
+#' macleish %>%
+#'   etl_extract() %>%
+#'   etl_transform() %>%
+#'   etl_load()
+#' whately <- macleish %>%
+#'   tbl("whately") 
+#'   
+#' whately %>%
+#'   summarize(N = n(), avg_temp = mean(Temp_C_Avg))
+
+etl_extract.etl_macleish <- function(obj, ...) {
+  url <- "https://scidb.smith.edu/~macleish/WhatelyMet_Met_10min.dat"
+  download.file(url, paste0(attr(obj, "raw_dir"), "/WhatelyMet_Met_10min.dat"))
+  invisible(obj)
+}
+
+#' @rdname etl_extract.etl_macleish
+#' @importFrom lubridate ymd_hms
+#' @export
 
 
-etl_extract.etl_macleish <- function (obj, ...) {
-  dat <- getURL("https://scidb.smith.edu/~macleish/WhatelyMet_Met_10min.dat")
-  x <- read.csv(textConnection(dat), skip = 1, stringsAsFactors = FALSE)
+etl_transform.etl_macleish <- function(obj, ...) {
+  lcl <- paste0(attr(obj, "raw_dir"), "/WhatelyMet_Met_10min.dat")
+  x <- read.csv(lcl, skip = 1, stringsAsFactors = FALSE)
   metadata <- head(x, 2)
   out <- x[-(1:2), -2]
   out <- out %>%
@@ -19,5 +43,21 @@ etl_extract.etl_macleish <- function (obj, ...) {
     mutate_(Press_mb_Avg = ~as.numeric(Press_mb_Avg)) %>%
     mutate_(SlrW_Avg = ~as.numeric(SlrW_Avg)) %>%
     mutate_(Rain_mm_Tot = ~as.numeric(Rain_mm_Tot))
-  return(out)
+  write.csv(out, file = paste0(attr(obj, "load_dir"), "/whately.csv"), row.names = FALSE)
+  invisible(obj)
+}
+
+#' @rdname etl_extract.etl_macleish
+#' @importFrom DBI dbWriteTable
+#' @export
+
+etl_load.etl_macleish <- function(obj, ...) {
+  data <- read.csv(paste0(attr(obj, "load_dir"), "/whately.csv"))
+  # write the table to the DB
+  message("Writing whately data to the database...")
+  if (DBI::dbWriteTable(obj$con, "whately", data, overwrite = TRUE, row.names = FALSE, ...)) {
+    message("Data was successfully written to database.")
+    message(DBI::dbListTables(obj$con))
+  }
+  invisible(obj)
 }
