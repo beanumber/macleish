99b92468a021357be94f51091931f7c701113cfe
Ben Baumer <ben.baumer@gmail.com>
2016-03-21 11:57:25 -0400

    updated spatial data and README

--- data-raw/macleish.R
+++ data-raw/macleish.R
@@ -27,21 +27,21 @@ list.files(dsn)
 ogrListLayers(dsn)
 ogrInfo(dsn, layer = "Trails")
 
-layers <- c("Key Landmarks", "VernalPool", "VegType_2009_20101112", "Streams", 
-            "ChallengeCourse_Elements", "MacLeishBoundary", "Reservoir", 
-            "Property Boundary", "Buildings", "Steep Slopes", "Wetlands",
-            "Trails")
+layers <- c("Key Landmarks", "VegType_2009_20101112", "Streams", 
+            "ChallengeCourse_Elements", "Reservoir", 
+            "Buildings", "Wetlands",
+            "Trails", "Slope2016", "SCHOOLS_PT", "2016_MacLeish_Boundary")
 macleish_layers <- lapply(layers, readOGR, dsn = dsn)
 
-names(macleish_layers) <- c("landmarks", "vernal_pool", "forests", "streams",
-                            "challenge_courses", "macleish_boundary", "reservoir", 
-                            "property_boundary", "buildings", "slopes", "wetlands",
-                            "trails")
+names(macleish_layers) <- c("landmarks", "forests", "streams",
+                            "challenge_courses", "reservoir", 
+                            "buildings", "wetlands",
+                            "trails", "slopes", "schools", "boundary")
 
 lapply(macleish_layers, proj4string)
 
-# fix the Steep Slopes projection string
-proj4string(macleish_layers[[10]]) <- proj4string(macleish_layers[[1]])
+# fix the projection string
+# proj4string(macleish_layers[[10]]) <- proj4string(macleish_layers[[1]])
 
 macleish_layers <- lapply(macleish_layers, spTransform, CRSobj = CRS("+init=epsg:4326"))
 
