e4625a8f88b1181785fe5bbb8f3d5f7fd6f37b45
beanumber <ben.baumer@gmail.com>
2018-04-10 22:29:37 -0400

    fixed test

--- tests/testthat/test-macleish.R
+++ tests/testthat/test-macleish.R
@@ -7,7 +7,7 @@ test_that("data integrity", {
   expect_is(whately_2015, "tbl_df")
   expect_is(orchard_2015, "tbl_df")
   expect_equal(nrow(whately_2015), 6*24*365)
-  expect_length(macleish_layers, 10)
+  expect_length(macleish_layers, 12)
   macleish_layers %>%
     sapply(FUN = sf::st_crs) %>%
     t() %>%
