6f0941b96e8911e1262badfd9bebcb19474694a7
Ben Baumer <ben.baumer@gmail.com>
2020-03-31 16:56:37 -0400

    hopefully fixed test

--- tests/testthat/test-macleish.R
+++ tests/testthat/test-macleish.R
@@ -8,10 +8,9 @@ test_that("data integrity", {
   expect_is(orchard_2015, "tbl_df")
   expect_equal(nrow(whately_2015), 6*24*365)
   expect_length(macleish_layers, 12)
-  macleish_layers %>%
+  epsg <- macleish_layers %>%
     purrr::map(sf::st_crs) %>%
-    purrr::map_int(`$`, epsg) %>%
-    `==`(4326) %>%
-    all() %>%
-    expect_true()
+    purrr::map_int(`$`, "epsg")
+  expect_true(all(epsg == 4326))
+  
 })
