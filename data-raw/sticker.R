faa1e945f0891344991d8d1965522bb28690fda5
beanumber <ben.baumer@gmail.com>
2020-08-31 08:58:08 -0400

    cleanup after #31

--- data-raw/sticker.R
+++ data-raw/sticker.R
@@ -20,8 +20,8 @@ macleish_sticker <- p %>%
     p_size = 24, s_x = 1, s_y = .75, s_width = 1.3, s_height = 1.3, h_size = 2,
     white_around_sticker = TRUE,
     url = "github.com/beanumber/macleish", u_size = 4
-  ) + 
-  geom_pkgname(package = "ma", color = "dodgerblue", y = 1.53, size = 24, x = 0.59)
+  ) #+ 
+#  geom_pkgname(package = "ma", color = "dodgerblue", y = 1.53, size = 24, x = 0.587)
 
 save_sticker(here::here("data-raw", "hex_macleish.png"), macleish_sticker)
 
