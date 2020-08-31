ea2e5fe66ea674224504dd2c01897843a13efd82
rudeboybert <albert.ys.kim@gmail.com>
2020-08-31 06:56:03 -0400

    Edit hex sticker

--- data-raw/sticker.R
+++ data-raw/sticker.R
@@ -14,10 +14,10 @@ p <- ggplot(macleish_layers[["boundary"]]) +
 
 macleish_sticker <- p %>%
   sticker(
-    package = "macleish", p_color = "#5F6B65", h_color = "#5F6B65", h_fill = "#FFFFFF",
+    package = "macleish", p_color = "#19233A", h_color = "#19233A", h_fill = "#DEA748",
     p_y = 1.53,
     filename = here::here("data-raw", "hex_macleish.png"),
-    p_size = 24, s_x = 1, s_y = .75, s_width = 1.3, s_height = 1.3,
+    p_size = 24, s_x = 1, s_y = .75, s_width = 1.3, s_height = 1.3, h_size = 2,
     white_around_sticker = TRUE,
     url = "github.com/beanumber/macleish", u_size = 4
   ) + 
