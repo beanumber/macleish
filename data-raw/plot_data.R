b92850ee9d02a9a697a9a6725aba9a2b6a68c0ca
Dominique Kelly <djkelly@smith.edu>
2021-06-29 10:31:55 -0400

    Added .Renviron to update R, fixed error in description, updated documentation, fixed variable names and missing values in dataset

--- data-raw/plot_data.R
+++ data-raw/plot_data.R
@@ -14,23 +14,28 @@ tree_diameter2 <- read_excel("data-raw/MacLeish Data_Plot MC-02.xlsx")
 #Plot Data 1
 #Creating year and height column 
 tree_diameter1 <- tree_diameter1 %>%
-  pivot_longer(c(`Fall'15`, `Fall'14`,`Fall'13`,`Fall'12`,`Fall'11`,`Fall'10`), names_to = "Year", values_to = "Diameter(cm)")
+  pivot_longer(c(`Fall'15`, `Fall'14`,`Fall'13`,`Fall'12`,`Fall'11`,`Fall'10`), names_to = "year", values_to = "diameter")
 
 #Separating fall and year into new columns 
 tree_diameter1 <- tree_diameter1 %>%
-  separate(Year, c("Semester", "Year"), sep = 4)
+  separate(year, c("semester", "year"), sep = 4)
 
 #Changing abbreviated year into full year 
-tree_diameter1$Year[tree_diameter1$Year == "'15"] <- "2015"
-tree_diameter1$Year[tree_diameter1$Year == "'14"] <- "2014"
-tree_diameter1$Year[tree_diameter1$Year == "'13"] <- "2013"
-tree_diameter1$Year[tree_diameter1$Year == "'12"] <- "2012"
-tree_diameter1$Year[tree_diameter1$Year == "'11"] <- "2011"
-tree_diameter1$Year[tree_diameter1$Year == "'10"] <- "2010"
+tree_diameter1$year[tree_diameter1$year == "'15"] <- "2015"
+tree_diameter1$year[tree_diameter1$year == "'14"] <- "2014"
+tree_diameter1$year[tree_diameter1$year == "'13"] <- "2013"
+tree_diameter1$year[tree_diameter1$year == "'12"] <- "2012"
+tree_diameter1$year[tree_diameter1$year == "'11"] <- "2011"
+tree_diameter1$year[tree_diameter1$year == "'10"] <- "2010"
 
 #Changing value "DEAD" to NA for diameter
 #Tree diameter didn't increase or decrease over time 
-tree_diameter1$`Diameter(cm)`[tree_diameter1$`Diameter(cm)` == "DEAD"] <- "NA"
+tree_diameter1$`diameter`[tree_diameter1$`diameter` == "DEAD"] <- "NA"
+
+#Changing variable names to lowercase and removing numerical units 
+tree_diameter1 <- tree_diameter1 %>%
+  rename(notes = `Notes:`, module = `Module#`, tag = `Tag#`) %>% 
+  rename_with(tolower)
 
 View(tree_diameter1)
 
@@ -38,23 +43,31 @@ View(tree_diameter1)
 
 #Creating year and height column 
 tree_diameter2 <- tree_diameter2 %>%
-  pivot_longer(c(`FALL 2016`, `Fall'12`,`Fall'11`,`Fall'10`,`Fall'09`), names_to = "Year", values_to = "Diameter(cm)")
+  pivot_longer(c(`FALL 2016`, `Fall'12`,`Fall'11`,`Fall'10`,`Fall'09`), names_to = "year", values_to = "diameter")
 
 #Separating fall and year into new columns 
 tree_diameter2 <- tree_diameter2 %>%
-  separate(Year, c("Semester", "Year"), sep = 4)
+  separate(year, c("semester", "year"), sep = 4)
 
 #Changing abbreviated year into full year 
-tree_diameter2$Year[tree_diameter2$Year == "'09"] <- "2009"
-tree_diameter2$Year[tree_diameter2$Year == "'10"] <- "2010"
-tree_diameter2$Year[tree_diameter2$Year == "'11"] <- "2011"
-tree_diameter2$Year[tree_diameter2$Year == "'12"] <- "2012"
+tree_diameter2$year[tree_diameter2$year == "'09"] <- "2009"
+tree_diameter2$year[tree_diameter2$year == "'10"] <- "2010"
+tree_diameter2$year[tree_diameter2$year == "'11"] <- "2011"
+tree_diameter2$year[tree_diameter2$year == "'12"] <- "2012"
 
 #Changing fall to lowercase
-tree_diameter2$Semester[tree_diameter2$Semester == "FALL"] <- "Fall"
+tree_diameter2$semester[tree_diameter2$semester == "FALL"] <- "Fall"
 
 #Changing value "DEAD" to NA for height 
-tree_diameter2$`Diameter(cm)`[tree_diameter2$`Diameter(cm)` == "DEAD"] <- "NA"
+tree_diameter2$diameter[tree_diameter2$diameter == "DEAD"] <- "NA"
+
+#Changing variable names to lowercase and removing numerical units
+tree_diameter2 <- tree_diameter2 %>%
+  rename(notes = `Notes:`, module = `Module#`, tag = `Tag#`) %>% 
+  rename_with(tolower)
+
+#Replace species listed as "???" with NA
+tree_diameter2$species[tree_diameter2$species == "???"] <- "NA"
 
 View(tree_diameter2)
 
